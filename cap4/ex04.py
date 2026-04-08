591a5e5842da715af37dbc6b5682b4cd93a20a89
Artur Lima Melo <arturnatalmelo@gmail.com>
2026-04-08 10:27:37 -0300

    fix: ex04 math

--- cap4/ex04.py
+++ cap4/ex04.py
@@ -1,7 +1,8 @@
 valorFinal = float(input("Digiteo valor final de vneda de um automóvel: "))
-ICMS = valorFinal * 0.18
-IPI = valorFinal * 0.13
-PIS = valorFinal * 0.014
-Cofins = valorFinal * 0.076
+valorCusto = valorFinal / 1.4
+ICMS = valorCusto * 0.18
+IPI = valorCusto * 0.13
+PIS = valorCusto * 0.014
+Cofins = valorCusto * 0.076
 valorLiquido = valorFinal - ICMS - IPI - PIS - Cofins
 print("ICMS:", "{:.2f}".format(ICMS),"\nIPI:","{:.2f}".format(IPI),"\nPIS:","{:.2f}".format(PIS),"\nCofins:","{:.2f}".format(Cofins),"\nValor sem impostos:", valorLiquido)
